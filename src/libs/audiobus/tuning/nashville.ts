e6ae37fdb44ed8a32ada0665e20214efa206d88a
zen <designerzen@gmail.com>
2025-09-17 21:02:55 +0100

    feat: scales constant names

--- src/libs/audiobus/tuning/nashville.ts
+++ src/libs/audiobus/tuning/nashville.ts
@@ -1,23 +1,11 @@
 // Nashville Number System
+import { SCALE_MAJOR, SCALE_IONIAN, SCALE_NATURAL_MINOR, SCALE_AEOLIAN, SCALE_DORIAN, SCALE_PHRYGIAN, SCALE_LYDIAN, SCALE_MIXOLYDIAN, SCALE_LOCRIAN, SCALE_HARMONIC_MINOR, SCALE_MELODIC_MINOR } from './scales.ts'
 
 // List of notes in order (for C major, but can be rotated for other keys)
 const NOTES = [
     'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
 ]
 
-
-// Scale type keys as constants
-export const SCALE_MAJOR = 'major'
-export const SCALE_IONIAN = 'ionian'
-export const SCALE_NATURAL_MINOR = 'naturalMinor'
-export const SCALE_AEOLIAN = 'aeolian'
-export const SCALE_DORIAN = 'dorian'
-export const SCALE_PHRYGIAN = 'phrygian'
-export const SCALE_LYDIAN = 'lydian'
-export const SCALE_MIXOLYDIAN = 'mixolydian'
-export const SCALE_LOCRIAN = 'locrian'
-export const SCALE_HARMONIC_MINOR = 'harmonicMinor'
-export const SCALE_MELODIC_MINOR = 'melodicMinor'
 
 // Interval patterns (in semitones) for common scales and modes
 const SCALE_PATTERNS: Record<string, number[]> = {
@@ -81,7 +69,6 @@ export const getScale = (rootNote: string, scaleType: string = 'major'): string[
 // getScale('C', 'major') // [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ]
 // getScale('A', 'naturalMinor') // [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
 // nashvilleToChord(4, 'G', 'mixolydian') // 'C'
-
 
 /**
  * Maps a Nashville Number System degree (1-7) to the corresponding chord in the given key and scale type.
---
+++ src/libs/audiobus/tuning/scales.ts
@@ -0,0 +1,14 @@
+
+
+// Scale type keys as constants
+export const SCALE_MAJOR = 'major'
+export const SCALE_IONIAN = 'ionian'
+export const SCALE_NATURAL_MINOR = 'naturalMinor'
+export const SCALE_AEOLIAN = 'aeolian'
+export const SCALE_DORIAN = 'dorian'
+export const SCALE_PHRYGIAN = 'phrygian'
+export const SCALE_LYDIAN = 'lydian'
+export const SCALE_MIXOLYDIAN = 'mixolydian'
+export const SCALE_LOCRIAN = 'locrian'
+export const SCALE_HARMONIC_MINOR = 'harmonicMinor'
+export const SCALE_MELODIC_MINOR = 'melodicMinor'
